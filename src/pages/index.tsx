04f3f871c7b9a2dc92190dcd4052342a83ef60a8
hatano299 <113757473+hatano299@users.noreply.github.com>
2026-07-12 22:37:58 +0900

    chore: whoamiページ関連を削除 (#20)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Layout from "@/components/common/Layout";
 import Container from "@/components/common/parts/Container";
-import WhoamiCard from "@/components/works/WhoamiCard";
 import WorkCard from "@/components/works/WorkCard";
 import { useWorks } from "@/hooks/works/useWorks";
 import { ROUTES } from "@/lib/data/routes";
@@ -13,9 +12,6 @@ const TopPage: NextPage = () => {
     <Layout path={ROUTES.WORKS} title="WORKS">
       <Container maxWidth="max-w-7xl flex justify-center">
         <div className="mt-8 columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-4">
-          <div className="break-inside-avoid">
-            <WhoamiCard />
-          </div>
           {works.map((work) => (
             <div key={work.id} className="mt-4 break-inside-avoid">
               <WorkCard work={work} />
