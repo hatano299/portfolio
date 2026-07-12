2b8eb7b017080e3999361f27bba0a315d413f2e5
hatano299 <hatano.yota35@gmail.com>
2026-07-12 23:56:21 +0900

    feat: datasourceをjson管理に変更

--- src/components/works/WorkCard.tsx
+++ src/components/works/WorkCard.tsx
@@ -11,20 +11,20 @@ const WorkCard = ({ work }: Props): React.ReactElement => {
     <div className="w-2xs rounded-2xl border-2 border-double border-[#AAAAAA] p-6 shadow-lg shadow-gray-500/30">
       <h3 className="text-display">WORK</h3>
       <Image
-        src={work.imageUrl}
+        src={work.image_url}
         alt={`${work.title}のイメージ`}
         width={288}
         height={160}
         className="rounded-2xl"
       />
       <h3 className="text-card-heading">{work.title}</h3>
-      <p className="text-base font-body">{work.createdAt}</p>
-      <p className="text-base font-body">{work.description}</p>
+      <p className="text-card-body">{work.created_at}</p>
+      <p className="text-card-body">{work.description}</p>
       <div className="flex justify-end">
         <LinkButton
           variant="primary"
           label="view app"
-          linkUrl={work.linkUrl}
+          linkUrl={work.link_url}
           className="mt-2 w-30 border-[#AAAAAA]"
         />
       </div>
