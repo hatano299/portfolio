e7626ee02c549f7b63cd745d12242d8451d4a47a
hatano299 <hatano.yota35@gmail.com>
2026-07-16 03:23:37 +0900

    feat: タブ名の変更

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -7,7 +7,7 @@ const Header = (props: Props): React.ReactElement => {
 
   return (
     <header className="pt-8 pb-14 flex justify-center">
-      <p className="text-display">{title}</p>
+      <p className="text-display uppercase">{title}</p>
     </header>
   );
 };
--- src/lib/data/metaData.ts
+++ src/lib/data/metaData.ts
@@ -1,4 +1,4 @@
 // Layoutのデフォルト設定
-export const APP_NAME = "Hatano's Portfolio";
+export const APP_NAME = "hatano";
 export const APP_ROOT_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "";
-export const APP_DEFAULT_DESCRIPTION = "ハタノ<Webエンジニア>のポートフォリオサイトです。";
+export const APP_DEFAULT_DESCRIPTION = "hatano yota のポートフォリオサイトです。";
